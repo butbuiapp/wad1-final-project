e2d06f1bf0e0b37af96c887db764df5afd1c814f
BNB <butbuiapp@gmail.com>
2023-09-23 20:44:17 -0500

    fix show chatbot after login

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,11 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import { useOutlet, useNavigate } from "react-router-dom";
+import Chatbot from './Chatbot';
 
 function Layout() {
   const outlet = useOutlet();
   const navigate = useNavigate();
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleChat = () => {
+    setIsVisible(!isVisible);
+  }
 
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -19,6 +25,8 @@ function Layout() {
       <Header />
       {outlet}
       <Footer />
+      <button className='chatbot-btn' onClick={toggleChat}>Need help?</button>
+      {isVisible && <Chatbot />}
     </div>
   )
 }
