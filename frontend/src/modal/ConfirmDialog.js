6ea8b417a9b82c45d54646c0facc5093a23a5218
BNB <butbuiapp@gmail.com>
2023-09-24 11:20:31 -0500

    add tests

--- frontend/src/modal/ConfirmDialog.js
+++ frontend/src/modal/ConfirmDialog.js
@@ -15,8 +15,10 @@ function ConfirmDialog(props) {
       <div className='backdrop'>
         <div className='modal'>
           <p>{props.message}</p>
-          <button className="btn btn-edit small" onClick={confirmHandler}>Confirm</button>
-          <button className="btn btn-cancel small ml" onClick={cancelHandler}>Cancel</button>          
+          <button className="btn btn-edit small" name="confirm"
+            onClick={confirmHandler} >Confirm</button>
+          <button className="btn btn-cancel small ml" name="cancel"
+            onClick={cancelHandler} >Cancel</button>          
         </div>
       </div>
     </div>
